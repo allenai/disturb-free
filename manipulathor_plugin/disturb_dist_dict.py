a3a44ecb15967678ac28761589b59f09fae60f4f
Tianwei Ni <twni2016@gmail.com>
2021-12-14 20:34:38 -0800

    black

--- manipulathor_plugin/disturb_dist_dict.py
+++ manipulathor_plugin/disturb_dist_dict.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 import ai2thor.controller
+
 ENV_ARGS = dict(
     gridSize=0.25,
     width=224,
@@ -18,7 +19,7 @@ ENV_ARGS = dict(
 )
 
 commit_id = "a84dd29471ec2201f583de00257d84fac1a03de2"
-ENV_ARGS['commit_id'] = commit_id
+ENV_ARGS["commit_id"] = commit_id
 controller = ai2thor.controller.Controller(**ENV_ARGS)
 
 kitchens = [f"FloorPlan{i}" for i in range(1, 31)]
@@ -29,11 +30,13 @@ bathrooms = [f"FloorPlan{400 + i}" for i in range(1, 31)]
 scenes = kitchens
 #  + living_rooms + bedrooms + bathrooms
 
+
 def make_dict_from_object(object_list):
     result = {}
     for obj in object_list:
-        result[obj['objectId']] = dict(position=obj['position'])
+        result[obj["objectId"]] = dict(position=obj["position"])
     return result
+
 
 def position_distance(s1, s2):
     position1 = s1["position"]
@@ -45,8 +48,9 @@ def position_distance(s1, s2):
     ) ** 0.5
     return dist
 
+
 def object_vibration_list(d1, d2):
-    vib = {'object': [], "dist": []}
+    vib = {"object": [], "dist": []}
     for object in d1.keys():
         vib["object"].append(object)
         vib["dist"].append(position_distance(d1[object], d2[object]))
@@ -60,10 +64,10 @@ for scene in scenes:
     controller.reset(scene)
     total = 200
 
-    initial_objects = make_dict_from_object(controller.last_event.metadata['objects'])
-    for i in range(total):            
-        controller.step('AdvancePhysicsStep')
-    final_objects = make_dict_from_object(controller.last_event.metadata['objects'])
+    initial_objects = make_dict_from_object(controller.last_event.metadata["objects"])
+    for i in range(total):
+        controller.step("AdvancePhysicsStep")
+    final_objects = make_dict_from_object(controller.last_event.metadata["objects"])
 
     vib = object_vibration_list(initial_objects, final_objects)
     df = pd.DataFrame.from_dict(vib)
@@ -72,4 +76,6 @@ for scene in scenes:
     results.append(df)
 
 results = pd.concat(results)
-results.to_csv("projects/manipulathor_disturb_free/manipulathor_plugin/vibrations.csv", index=False)
+results.to_csv(
+    "projects/manipulathor_disturb_free/manipulathor_plugin/vibrations.csv", index=False
+)
